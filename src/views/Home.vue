60bf9bfac89e036d3dce2222b93c4347d9f3cb58
gabriel.caus <gabriel.caus@codenapp.com>
2023-06-01 18:10:57 -0300

    Login spotify-api

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,23 @@
 <template>
     <div>
-        <p>teste</p>
+        <button @click="login">Login</button>
     </div>
 </template>
 
 <script>
     export default {
-        
+        data() {
+            return {
+                clientId: 'aa881f98bb814ef09cb60b5e5ee9c87a',
+                scope: 'user-top-read',
+                redirectUrl: 'http://127.0.0.1:5173/my-allura'
+            }
+        },
+        methods: {
+            login() {
+                location.href = `https://accounts.spotify.com/authorize?response_type=code&client_id=${this.clientId}&scope=${this.scope}&redirect_uri=${this.redirectUrl}`
+            }
+        }
     }
 </script>
 
