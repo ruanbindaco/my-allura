bf417fc967b11976c85feb4c471859b20a76905b
Ruan Bindaco <ruanazsxdc123@gmail.com>
2023-06-01 17:58:54 -0300

    Tela My allura

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import AlluraList from '../views/AlluraList.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -8,6 +9,11 @@ const router = createRouter({
       name: 'home',
       component: Home
     },
+    {
+      path: '/my-allura',
+      name: 'list',
+      component: AlluraList
+    }
   ]
 })
 
---
+++ src/views/AlluraList.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="container">
+    <div class="allura-list">
+      <div class="artists">
+        <div class="names">
+          ARTISTA 1 ARTISTA 1 ARTISTA 1 ARTISTA 1 ARTISTA 1 ARTISTA 1 ARTISTA 1
+          ARTISTA 1 ARTISTA 1 ARTISTA 1 ARTISTA 1 ARTISTA 1 ARTISTA 1 ARTISTA 1
+          ARTISTA 1 ARTISTA 1 ARTISTA 1 ARTISTA 1 ARTISTA 1 ARTISTA 1 ARTISTA 1
+          ARTISTA 1 ARTISTA 1 ARTISTA 1 ARTISTA 1 ARTISTA 1 ARTISTA 1 ARTISTA 1
+          ARTISTA 1 ARTISTA 1 ARTISTA 1 ARTISTA 1
+        </div>
+        <div class="link">www.google.com</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+@import url("https://fonts.googleapis.com/css2?family=Slabo+27px&display=swap");
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .allura-list {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-image: url("../assets/images/my-allura.png");
+    background-size: cover;
+    max-width: 500px;
+    width: 100%;
+    height: 500px;
+    transform: scale(0.9);
+
+    .artists {
+      display: flex;
+      justify-content: space-between;
+      flex-direction: column;
+      height: 100%;
+
+      .names {
+        display: flex;
+        justify-content: center;
+        padding: 135px 45px 0;
+        font-family: "Slabo", serif;
+        text-align: justify;
+        font-size: 20px;
+        color: #fff;
+      }
+
+      .link {
+        display: flex;
+        justify-content: center;
+        padding-bottom: 72px;
+        font-family: "Slabo", serif;
+        text-align: justify;
+        font-size: 20px;
+        color: #fff;
+      }
+    }
+  }
+}
+
+@media (max-width: 530px) {
+  .container {
+    .allura-list {
+      background-size: contain;
+      background-repeat: no-repeat;
+      height: 500px;
+
+      .artists {
+        justify-content: start;
+        gap: 20px;
+        .names {
+          padding-top: 100px;
+          font-size: 16px;
+        }
+
+        .link {
+          padding-bottom: 30px;
+          font-size: 10px;
+        }
+      }
+    }
+  }
+}
+</style>
